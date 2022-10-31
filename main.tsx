63454d8dcab96f995b7ec5d83283a63bfbd3c5d9
Vladyslav <fareomg@gmail.com>
2022-10-31 04:06:52 +0200

    Update avatar

--- main.tsx
+++ main.tsx
@@ -7,8 +7,8 @@ blog({
   // header: <header>Your custom header</header>,
   // section: <section>Your custom section</section>,
   // footer: <footer>Your custom footer</footer>,
-  // avatar: "./assets/avatar.jpg",
-  // avatarClass: "rounded-full",
+  avatar: "./assets/avatar.jpg",
+  avatarClass: "rounded-full",
   author: "Vladyslav Silich",
   // cover: "./assets/cover.jpg",
   links: [
